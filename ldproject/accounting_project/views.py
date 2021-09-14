cb408ac674cf96c3db75b66db3bb983f23131b57
joeygracesabusido@gmail.com <joeygracesabusido@gmail.com>
2021-09-14 17:27:29 +0800

    edited 09.14.21 5:27PM

--- ldproject/accounting_project/views.py
+++ ldproject/accounting_project/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -26,8 +27,15 @@ def getNote(request,pk):
 
 @api_view(['POST'])
 def post_data(request):
-    
+    # data = request.data
+    # notes = note.objects.create(
+    #     body=data['body']
+    # )
+    # serializer = noteSeralizers(notes, many=False)
+    # return Response(serializer.data)
+
     serializer = noteSeralizers(data=request.data)
-    if serializer.is_valid(raise_exception=True):
+    if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
